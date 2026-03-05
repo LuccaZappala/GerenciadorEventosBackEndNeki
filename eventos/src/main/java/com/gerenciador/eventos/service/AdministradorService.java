18ff0cf030f7f8edb4fc39447e25b5317245cc82
unknown <luccazappala@gmail.com>
2026-03-05 16:07:22 -0300

    feat: add SecurityConfig com passwordEncoder

--- eventos/src/main/java/com/gerenciador/eventos/service/AdministradorService.java
+++ eventos/src/main/java/com/gerenciador/eventos/service/AdministradorService.java
@@ -1,6 +1,7 @@
 package com.gerenciador.eventos.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.gerenciador.eventos.dto.AdministradorCadastroDTO;
@@ -14,12 +15,18 @@ public class AdministradorService {
 	@Autowired
 	AdministradorRepository repository;
 	
+	@Autowired
+	PasswordEncoder passwordEncoder;
+	
 	public AdministradorDTO cadastrar(AdministradorCadastroDTO dto) {
 		
 		Administrador admin = new Administrador();
 		admin.setNome(dto.getNome());
 		admin.setEmail(dto.getEmail());
 		admin.setSenha(dto.getSenha());
+		
+		String senhaCriptografada = passwordEncoder.encode(dto.getSenha());
+		admin.setSenha(senhaCriptografada);
 		
 		Administrador salvo = repository.save(admin);
 		
